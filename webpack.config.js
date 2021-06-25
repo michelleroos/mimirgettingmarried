1f2bcef9b07cbd08c28585bb66db6e083d77fd8b
michelleroos <hello.michelleroos@gmail.com>
2021-06-25 12:52:56 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+module.exports = {
+    entry: './frontend/entry.jsx',
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+}
+
+// const path = require('path');
+
+// module.exports = {
+//     context: __dirname,
+//     entry: './frontend/pokedex.jsx',
+//     output: {
+//         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//         filename: 'bundle.js'
+//     },
+//     resolve: {
+//         extensions: ['.js', '.jsx', '*']
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.jsx?$/,
+//                 exclude: /(node_modules)/,
+//                 use: {
+//                     loader: 'babel-loader',
+//                     query: {
+//                         presets: ['@babel/env', '@babel/react']
+//                     }
+//                 },
+//             }
+//         ]
+//     },
+//     devtool: 'source-map'
+// };
