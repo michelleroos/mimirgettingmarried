02e028db1ea8a97efb5acb0f3c6b1839562f1c4e
michelleroos <hello.michelleroos@gmail.com>
2021-06-25 16:06:12 -0400

    users_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,34 @@
 class Api::UsersController < ApplicationController
+
+        def create
+        @user = User.new(user_params)
+        if @user.save
+            log_in!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @user = current_user
+        if @user
+            render :show
+        else
+            render json: ['User not found'], status: 404
+        end
+    end
+
+    # def update
+    # end
+
+    # def destroy
+    # end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:email, :password, :first_name, :last_name)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ApplicationRecord
+
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    validates :password_digest, presence: true #:first_name, :last_name,
+    attr_reader :password
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            return user
+        end
+        nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
 end
