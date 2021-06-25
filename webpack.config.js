e26c035c7c41441435b9357eea2a42117e45a610
michelleroos <hello.michelleroos@gmail.com>
2021-06-25 15:11:22 -0400

    webpack fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 module.exports = {
+    context: __dirname,
     entry: './frontend/entry.jsx',
     output: {
-        // path: path.resolve(__dirname),
-        // filename: 'bundle.js'
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
     },
     module: {
         rules: [
@@ -18,41 +20,9 @@ module.exports = {
                     options: {
                         presets: ['@babel/env', '@babel/react']
                     }
-                }
+                },
             }
         ]
     },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.js', '.jsx', '*']
-    }
-}
-
-// const path = require('path');
-
-// module.exports = {
-//     context: __dirname,
-//     entry: './frontend/pokedex.jsx',
-//     output: {
-//         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-//         filename: 'bundle.js'
-//     },
-//     resolve: {
-//         extensions: ['.js', '.jsx', '*']
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.jsx?$/,
-//                 exclude: /(node_modules)/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                     query: {
-//                         presets: ['@babel/env', '@babel/react']
-//                     }
-//                 },
-//             }
-//         ]
-//     },
-//     devtool: 'source-map'
-// };
+    devtool: 'source-map'
+};
