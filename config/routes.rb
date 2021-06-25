22f547691aa180716101b8e9951beec1974ec6d5
michelleroos <hello.michelleroos@gmail.com>
2021-06-25 14:35:40 -0400

    static_pages#root controller and view + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
+# == Route Map
+#
+#      Prefix Verb   URI Pattern              Controller#Action
+#        root GET    /                        static_pages#root
+# api_session DELETE /api/session(.:format)   api/sessions#destroy {:format=>:json}
+#             POST   /api/session(.:format)   api/sessions#create {:format=>:json}
+#   api_users POST   /api/users(.:format)     api/users#create {:format=>:json}
+#    api_user GET    /api/users/:id(.:format) api/users#show {:format=>:json}
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "static_pages#root"
+
+  namespace :api, defaults: { format: :json } do
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:create, :show]
+  end
 end
