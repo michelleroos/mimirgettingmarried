5b2c48cd565ae8c2a310c26c1b46b8405dbe8e8d
michelleroos <hello.michelleroos@gmail.com>
2021-06-25 16:16:41 -0400

    session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,27 @@
 class Api::SessionsController < ApplicationController
+
+  def create 
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if @user
+      log_in!(@user)
+      render "api/users/show"
+    else
+      render json: ['ⓘ Unable to log in with provided credentials.'], status: 422
+    end
+  end
+
+  def destroy 
+    @user = current_user
+    if @user
+        log_out!
+        render "api/users/show"
+        # render {}
+    else
+        render json: ["No user is signed in"], status: 404
+    end
+  end
+
 end
