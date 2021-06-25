f55fce94b16139e7e1f3849a7c18ba9c92be17cb
michelleroos <hello.michelleroos@gmail.com>
2021-06-25 14:41:17 -0400

    lagger filed

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,1 +1,7 @@
-console.log('hello, world');
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+});
