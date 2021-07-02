200e9fedca22e270b6fc5c8e4066943d9ff489bc
michelleroos <hello.michelleroos@gmail.com>
2021-07-02 10:53:56 -0400

    upload file

--- frontend/components/Account/UploadForm.jsx
+++ frontend/components/Account/UploadForm.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function UploadForm() {
+
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError('Please select correct file type')
+        }
+    }
+
     return (
         <form>
-            <input type="file" />
+            <input type="file" onChange={changeHandler}/>
+            <div className="output">
+                { error && <div className="error">{ error }</div>}
+                { file && <div className="file">{ file.name }</div>}
+            </div>
         </form>
     )
 }
