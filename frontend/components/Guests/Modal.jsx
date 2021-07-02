0670ad2e95daa53bcbfb5a663285cbdb0765cc2a
michelleroos <hello.michelleroos@gmail.com>
2021-07-02 16:17:50 -0400

    modals

--- frontend/components/Guests/Modal.jsx
+++ frontend/components/Guests/Modal.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-export default function Modal({ selectedImg }) {
+export default function Modal({ selectedImg, setSelectedImg }) {
+
+    const handleClick = (e) => {
+        if (e.target.classList.contains('backdrop')) {
+            setSelectedImg(null);
+        }
+    }
     return (
-        <div className="backdrop">
+        <div className="backdrop" onClick={handleClick}>
             <img src={selectedImg} alt="enlarged pic" />
         </div>
     )
