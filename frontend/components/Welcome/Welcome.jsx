8e8462ab8e49748bd7b91c8005f58444904bd178
michelleroos <hello.michelleroos@gmail.com>
2021-11-18 10:05:49 -0500

    home page img

--- frontend/components/Welcome/Welcome.jsx
+++ frontend/components/Welcome/Welcome.jsx
@@ -15,12 +15,12 @@ export default function Welcome({ currentUser, pathname }) {
         {/* <h1>We're getting married!</h1>
       <h4>23 JULY 2022 | SATURDAY | PRAGUE</h4>
       <br /> */}
-        <div className="p">
+        {/* <div className="p">
           <p>Welcome to join our wedding! On this website you can RSVP, browse our guestlist, see information about the location and schedule, find common questions and answers on the FAQ and manage your account information. If you have any questions or concerns, please don't hesitate to reach out to Michelle on +1 (650) 933 2293.</p>
           <br />
           <p>We don't do engagement photos, but here are some of our favorite photos of us since we met in 2016 :)</p>
-        </div>
-        <UploadForm pathname={pathname} />
+        </div> */}
+        {/* <UploadForm pathname={pathname} /> */}
         <AMImageGrid />
       </div>
     )
