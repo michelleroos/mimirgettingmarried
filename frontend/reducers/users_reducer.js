4020e5b776c89af34d9191c8efc62b9f10f0c299
michelleroos <hello.michelleroos@gmail.com>
2021-11-25 23:53:41 -0500

    location

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,25 +1,25 @@
 // import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { RECEIVE_RSVP, RECEIVE_RSVP_ERRORS } from '../actions/rsvp_actions';
 import {
-    RECEIVE_CURRENT_USER,
-    LOGOUT_CURRENT_USER,
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER,
 } from '../actions/session_actions';
 
 const _nullUser = Object.freeze({
-    user: null
+  user: null
 });
 
 const usersReducer = (state = {}, action) => {
-    Object.freeze(state);
-    switch (action.type) {
-        case RECEIVE_CURRENT_USER:
-            // return Object.assign({}, state, { user: action.currentUser.user });
-            return { user: action.currentUser.user };
-        case LOGOUT_CURRENT_USER:
-            return _nullUser;
-        default:
-            return state;
-    }
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      // return Object.assign({}, state, { user: action.currentUser.user });
+      return { user: action.currentUser.user };
+    case LOGOUT_CURRENT_USER:
+      return _nullUser;
+    default:
+      return state;
+  }
 };
 
 export default usersReducer;
