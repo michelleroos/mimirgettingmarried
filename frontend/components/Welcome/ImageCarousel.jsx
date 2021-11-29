0b81276aae5da20af7295cfccf1cf93877410670
michelleroos <hello.michelleroos@gmail.com>
2021-11-29 14:09:18 -0500

    image carousel

--- frontend/components/Welcome/ImageCarousel.jsx
+++ frontend/components/Welcome/ImageCarousel.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import useFirestore from '../../hooks/useFirestore';
 
-export default function ImageCarousel({open, setOpen, index}) {
+export default function ImageCarousel({ open, setOpen, index }) {
 
   const { docs } = useFirestore('am-photos');
   const [currentImg, setCurrentImg] = useState(index);
   const length = docs.length || null;
-  // const [open, setOpen] = useState(false);
 
   const prevImg = () => {
     setCurrentImg(currentImg === 0 ? length - 1 : currentImg - 1)
@@ -21,40 +20,21 @@ export default function ImageCarousel({open, setOpen, index}) {
     return null;
   } else {
     return (
-      // <div id="welcome-container">
-        <div id="disclosure-modal-container">
-          <div id="modal-bg" onClick={() => setOpen(false)}>
-          </div>
-          {/* 
-          <div id="disclosure-modal">
-            <img src={img} />
-          </div> */}
-          {/* <div id="img-carousel">
-            <i className="fas fa-caret-left" onClick={nextImg}></i>
-            <i className="fas fa-caret-right" onClick={prevImg}></i>
-            {docs.map((img, idx) => {
-              return (
-                <div className={idx === currentImg ? "img-container active" : "img-container"} key={idx}>
-                  {idx === currentImg && (
-                    <img src={img.url} alt={img.id} className="carousel-img" />
-                  )}
-                </div>
-              )
-            })}
-          </div> */}
-          <div id="img-carousel">
-            <i className="fas fa-caret-left" onClick={nextImg}></i>
-            <i className="fas fa-caret-right" onClick={prevImg}></i>
-            {docs.map((img, idx) => {
-              if (idx === currentImg) {
-                return (
-                  <img src={img.url} alt={img.id} key={idx} className={idx === currentImg ? "img-container active" : "img-container"} />
-                )
-              }
-            })}
-          </div>
+      <div id="disclosure-modal-container">
+        <div id="modal-bg" onClick={() => setOpen(false)}>
         </div>
-      // </div>
+        <div id="img-carousel">
+          <i className="fas fa-caret-left" onClick={nextImg}></i>
+          <i className="fas fa-caret-right" onClick={prevImg}></i>
+          {docs.map((img, idx) => {
+            if (idx === currentImg) {
+              return (
+                <img src={img.url} alt={img.id} key={idx} className={idx === currentImg ? "img-container active" : "img-container"} />
+              )
+            }
+          })}
+        </div>
+      </div>
     )
   }
 }
