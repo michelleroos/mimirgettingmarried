c65e55feebefdb2ce5ef732513637332a3f423f9
michelleroos <hello.michelleroos@gmail.com>
2021-12-19 20:06:58 -0500

    faqs

--- frontend/components/Faq/Faq.jsx
+++ frontend/components/Faq/Faq.jsx
@@ -27,9 +27,47 @@ export default function Faq() {
     if (!expand[num]) setExpand({ ...expand, [num]: true });
   };
 
+  const expandAll = () => {
+    setExpand({
+      0: true,
+      1: true,
+      2: true,
+      3: true,
+      4: true,
+      5: true,
+      6: true,
+      7: true,
+      8: true,
+      9: true,
+      10: true,
+      11: true,
+    })
+  }
+
+  const collapseAll = () => {
+    setExpand({
+      0: false,
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+      5: false,
+      6: false,
+      7: false,
+      8: false,
+      9: false,
+      10: false,
+      11: false,
+    })
+  }
+
   const Faq = () => (
     <div id="faq-wrapper">
       <div id="faq-container">
+        <div id="set-all">
+          <i class="fas fa-expand-alt" onClick={() => expandAll()}></i>
+          <i class="fas fa-compress-alt" onClick={() => collapseAll()}></i>
+        </div>
         {faqData.map((data, idx) => {
           return (
             <div className="faq-item" key={idx}>
--- frontend/components/Faq/data.js
+++ frontend/components/Faq/data.js
@@ -7,51 +7,51 @@ const data = [
     a: "July is the warmest month in Prague, with an average high- temperature of 24°C(75.2°F) and an average low - temperature of 11.9°C(53.4°F).Of course, in the last few years the temperatures have been higher.July usually has the lowest humidity of the year, with an average relative humidity of 70 %."
 
   }, {
-    q: "Will the ceremony / mingle / reception be in the same place ?",
+    q: "Will the ceremony / mingle / reception be in the same place?",
     a: "All activities on Saturday will be in the same place and the same goes for Sunday."
   }, {
-    q: "Will the ceremony / drinks / reception be inside or outside ?",
+    q: "Will the ceremony / drinks / reception be inside or outside?",
     a: "Weather permitting, the mingle and ceremony will be outside.The dinner and band will be in an indoor / outdoor area and the late night party will be inside."
   }, {
-    q: "How do I get to the venue and around Prague ?",
+    q: "How do I get to the venue and around Prague?",
     a: "The venue is located in the city center so you can either walk or take an Uber.Please note that the wedding venue is perched on an elevation so there’s an incline walk to get there.If you are wearing heels or have reduced mobility, you might consider taking a taxi or Uber."
   }, {
-    q: "Is the venue accessible for people with restricted mobility ?",
+    q: "Is the venue accessible for people with restricted mobility?",
     a: "?"
   }, {
-    q: "Is there parking at the venue ? ",
+    q: "Is there parking at the venue? ",
     a: "?"
 
   }, {
-    q: "What time should I arrive ?",
+    q: "What time should I arrive?",
     a: "TBD"
 
   }, {
-    q: "Are kids welcome ?",
+    q: "Are kids welcome?",
     a: "Your young children are welcome at the wedding on Saturday, but Sunday is strictly + 18 years."
 
   }, {
-    q: "Can I bring a + 1 ?",
+    q: "Can I bring a + 1?",
     a: "No."
 
   }, {
-    q: "Will there be any vegan / vegetarian / special diet options ?",
+    q: "Will there be any vegan / vegetarian / special diet options?",
     a: "Yes, please let us know if you need to be accommodated."
 
   }, {
-    q: "Can I post on social media ?",
+    q: "Can I post on social media?",
     a: "Yes, of course! Just tag # so that we can save the photos.While you’re at it, please add photos to our Google Photos folder or send them to us via Apple Photos / DropBox / whichever way you want."
 
   }, {
-    q: "Where do you recommend that I book my stay ?",
+    q: "Where do you recommend that I book my stay?",
     a: "Prague 1."
 
   }, {
-    q: "When is the RSVP deadline ?",
-    a: "May ?"
+    q: "When is the RSVP deadline?",
+    a: "May?"
 
   }, {
-    q: "What should I do if I no longer can make it ?",
+    q: "What should I do if I no longer can make it?",
     a: "Please just let us know asap so that we can plan accordingly."
 
   }, {
