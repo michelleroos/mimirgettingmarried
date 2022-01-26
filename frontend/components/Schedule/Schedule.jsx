aa2966b5effb045d03c7ec463aee69bd2b86c897
michelleroos <hello.michelleroos@gmail.com>
2022-01-26 17:50:21 -0500

    schedule

--- frontend/components/Schedule/Schedule.jsx
+++ frontend/components/Schedule/Schedule.jsx
@@ -38,7 +38,7 @@ export default function Schedule() {
         <div class="day-schedule">
           <h2>Friday - 22 July</h2>
           <div className="detailed-schedule-container">
-            <div className="schedule-details" onClick={() => setFriday(true)}>
+            <div className="schedule-details" id="friday-modal" onClick={() => setFriday(true)}>
               <i class="fas fa-beer"></i>
               <h3>Meet & greet</h3>
               <p className="hover">TBD</p>
@@ -48,29 +48,29 @@ export default function Schedule() {
         <div class="day-schedule">
           <h2>Saturday - 23 July</h2>
           <div className="detailed-schedule-container">
-            <div className="schedule-details" onClick={() => setMingle(true)}>
+            <div className="schedule-details" id="saturday-mingle-modal" onClick={() => setMingle(true)}>
               <i class="fas fa-cocktail"></i>
               <h3>Mingle</h3>
               <p>15:00</p>
               <p className="hover">Villa Richter Fountain</p>
             </div>
-            <div className="schedule-details" onClick={() => setCeremony(true)}>
+            <div className="schedule-details" id="saturday-ceremony-modal" onClick={() => setCeremony(true)}>
               <i class="fas fa-heart"></i>
               <h3>Ceremony</h3>
               <p>16:00</p>
               <p className="hover">Villa Richter</p>
             </div>
-            <div className="schedule-details" onClick={() => setReception(true)}>
+            <div className="schedule-details" id="saturday-receotion-modal" onClick={() => setReception(true)}>
               <i class="fas fa-utensils"></i>
               <h3>Reception</h3>
               <p>18:00</p>
-              <p className="hover">X @ Villa Richter</p>
+              <p className="hover">Villa Richter</p>
             </div>
             <div className="schedule-details">
               <i class="fas fa-guitar"></i>
               <h3>Music</h3>
               <p>20:00</p>
-              <p>X @ Villa Richter</p>
+              <p>Villa Richter</p>
             </div>
             <div className="schedule-details">
               <i class="fas fa-hourglass-end"></i>
@@ -82,7 +82,7 @@ export default function Schedule() {
         <div class="day-schedule">
           <h2>Sunday - 24 July</h2>
           <div className="detailed-schedule-container">
-            <div className="schedule-details" onClick={() => setSunday(true)}>
+            <div className="schedule-details" id="sunday-modal" onClick={() => setSunday(true)}>
               <i class="fas fa-music"></i>
               <h3>Party continues</h3>
               <p>17:00 until late</p>
--- frontend/components/Schedule/Sunday.jsx
+++ frontend/components/Schedule/Sunday.jsx
@@ -4,8 +4,15 @@ export default function Sunday({ close }) {
   return (
     <div className="modal-bg" onClick={close}>
       <div className="modal">
-        <p>On Sunday afternoon we will be hosting a party with a Berlin based surprise DJ on Sunday at <a href="https://www.glassbar.cz/">Glass Bar</a>.</p>
-        <p>Click <a href="https://www.google.com/maps/place/Glass+Bar/@50.075451,14.4120057,17z/data=!3m1!4b1!4m5!3m4!1s0x470b94f6c339f665:0x89f8681b400807c9!8m2!3d50.0755656!4d14.4141744" target="_blank">here</a> for Glass Bar on Google Maps.</p>
+        <h2>Sunday - July 24</h2>
+        <h2>Party at GLASS Bar</h2>
+        <h3>On Sunday afternoon we will be hosting a party with a Berlin based surprise DJ on Sunday at <a href="https://www.glassbar.cz/">Glass Bar</a>.</h3>
+        <div className="schedule-img-container">
+          <img src="https://firebasestorage.googleapis.com/v0/b/gallery-23854.appspot.com/o/peg3772.jpeg?alt=media&token=c204e58b-6ce0-4f34-97f3-92220aa54371" alt="glass-bar" />
+          <img src="https://firebasestorage.googleapis.com/v0/b/gallery-23854.appspot.com/o/tancici-dum-3.jpeg?alt=media&token=887da6d7-65e5-42da-a02d-856d12ddda5c" alt="glass-bar" />
+          <img src="https://firebasestorage.googleapis.com/v0/b/gallery-23854.appspot.com/o/A901-10-Things-you-did-not-know-about-Dancing-House-Rooftop-Image-1-1024x682.jpeg?alt=media&token=672f40fe-da52-467e-88fb-d410c4074ba1" alt="glass-bar" />
+        </div>
+        <h3>Click <a href="https://www.google.com/maps/place/Glass+Bar/@50.075451,14.4120057,17z/data=!3m1!4b1!4m5!3m4!1s0x470b94f6c339f665:0x89f8681b400807c9!8m2!3d50.0755656!4d14.4141744" target="_blank">here</a> for Glass Bar on Google Maps.</h3>
       </div>
     </div>
   )
