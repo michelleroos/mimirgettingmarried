de17120b6182b3edfd29857fe406733ab7b42f04
michelleroos <hello.michelleroos@gmail.com>
2022-02-18 12:00:52 -0500

    images

--- frontend/components/Welcome/ImageCarousel.jsx
+++ frontend/components/Welcome/ImageCarousel.jsx
@@ -1,16 +1,9 @@
 import React, { useEffect, useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import useFirestore from '../../hooks/useFirestore';
 
 export default function ImageCarousel({ img, setImg, imgID }) {
-  // export default function ImageCarousel({ open, setOpen, index, img, setImg, imgID }) {
 
-  // const { docs } = useFirestore('am-photos');
-  // const [currentImg, setCurrentImg] = useState(index);
-  // const length = docs.length || null;
 
   const handleClick = (e) => {
-    // if (e.target.id.contains('modal-bg')) setImg(null);
     if (e.target.id === 'modal-bg') setImg(null);
   }
 
@@ -22,38 +15,4 @@ export default function ImageCarousel({ img, setImg, imgID }) {
   )
 
   return carousel;
-
-  // if (docs) {
-  //   return carousel;
-  // } else {
-  //   return null;
-  // }
-
-  // docs ? carousel : null;
-
-  // const prevImg = () => {
-  //   setCurrentImg(currentImg === 0 ? length - 1 : currentImg - 1)
-  // }
-
-  // const nextImg = () => {
-  //   setCurrentImg(currentImg === length - 1 ? 0 : currentImg + 1)
-  // }
-
-  // const carousel = (
-  //   <div id="disclosure-modal-container">
-  //     <div id="modal-bg" onClick={() => setOpen(false)}>
-  //     </div>
-  //     <div id="img-carousel">
-  //       <i className="fas fa-caret-left" onClick={nextImg}></i>
-  //       <i className="fas fa-caret-right" onClick={prevImg}></i>
-  //       {docs.map((img, idx) => {
-  //         if (idx === currentImg) {
-  //           return (
-  //             <img src={img.url} alt={img.id} key={idx} className={idx === currentImg ? "img-container active" : "img-container"} />
-  //           )
-  //         }
-  //       })}
-  //     </div>
-  //   </div>
-  // )
 }
