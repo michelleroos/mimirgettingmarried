bd48572c7169add026eaab6c6775691f4df42c6e
michelleroos <hello.michelleroos@gmail.com>
2022-02-22 18:30:21 -0500

    log out

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import UploadForm from '../UploadForm/UploadForm';
 
-export default function Navbar({ logOut }) {
+export default function Navbar() {
 
   const currentUserId = useSelector((state) => state.session.id);
+  const dispatch = useDispatch();
+  const [hover, setHover] = useState(false);
   const MenuItems = ['Rsvp', 'Schedule', 'Faq']
   const [sidebar, setSidebar] = useState(false);
 
@@ -15,9 +17,8 @@ export default function Navbar({ logOut }) {
         {/* <UploadForm /> */}
 
         <div id="logo" onClick={() => setSidebar(false)}>
-          <Link to="/">
-            <p id="logo">A & M</p>
-            <i className="fas fa-heart"></i>
+          <Link to="/" onMouseEnter={() => setHover(!hover)} onMouseLeave={() => setHover(!hover)}>
+            {hover ? <p id="logo">A & M</p> : <i className="fas fa-heart"></i>}
           </Link>
         </div>
 
@@ -33,10 +34,10 @@ export default function Navbar({ logOut }) {
               </li>
             )
           })}
-          {sidebar ? <li id="menu-links"><Link to="/" onClick={logOut}>Log out</Link></li> : null}
+          {sidebar ? <li id="menu-links"><Link to="/" onClick={() => dispatch(logOut())}>Log out</Link></li> : null}
         </ul>
 
-        {sidebar ? null : <div id="log-out"><Link to="/" onClick={logOut}>Log out</Link></div>}
+        {sidebar ? null : <div id="log-out"><Link to="/" onClick={() => dispatch(logOut())}>Log out</Link></div>}
       </div>
     )
   } else {
