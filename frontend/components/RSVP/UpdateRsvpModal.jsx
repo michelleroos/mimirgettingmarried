52041caf89446ca1a6d283009e29b0cda33423b6
michelleroos <hello.michelleroos@gmail.com>
2022-03-18 18:36:03 -0400

    update modal

--- frontend/components/RSVP/UpdateRsvpModal.jsx
+++ frontend/components/RSVP/UpdateRsvpModal.jsx
@@ -151,7 +151,7 @@ export default function UpdateRsvpModal({ rsvpReq, setUpdateRsvpModal, updateRsv
               </div>
             </div>
           </div>
-          <button className="rsvp-submit-btn" onClick={() => changeRsvp(rsvpReq)}>
+          <button className="rsvp-submit-btn" id="update-rsvp-btn"onClick={() => changeRsvp(rsvpReq)}>
             <p>Update RSVP</p>
             <i className="far fa-envelope"></i>
           </button>
