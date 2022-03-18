23d517b894426ed74957b16fcf0aa864bacc3bdd
michelleroos <hello.michelleroos@gmail.com>
2022-03-18 16:46:10 -0400

    schedule done

--- frontend/components/Schedule/Friday.jsx
+++ frontend/components/Schedule/Friday.jsx
@@ -7,8 +7,8 @@ export default function Friday({ close }) {
       <div className="schedule-modal" id="friday-modal">
         <h2>Friday</h2>
         <h3>Friday evening's activity and location is TBD but we will likely arrange a meet-up in a Czech pub for anyone that is around.</h3>
-        <h3>Vibe and dress code - casual.</h3>
         <img src="https://1.bp.blogspot.com/-3_5l3viHVJc/YKPiTYMTe9I/AAAAAAAAFN0/FCezW2Nt5_wBPt5h-hoalGgaLGSN4V9OACLcBGAsYHQ/s1422/Czech1-min.png" alt="beer-prague" />
+        <h3>Vibe and dress code - casual.</h3>
       </div>
     </div>
   )
--- frontend/components/Schedule/Sunday.jsx
+++ frontend/components/Schedule/Sunday.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Sunday({ close }) {
   return (
     <div className="modal-bg" onClick={close}>
-      <div className="schedule-modal">
+      <div className="schedule-modal" id="sunday-modal">
         {/* <h2>Sunday - July 24</h2> */}
         <h2>Sunday</h2>
         <h3>On Sunday afternoon we will be hosting a party with a Berlin based surprise DJ on Sunday at <a href="https://www.glassbar.cz/">Glass Bar</a>.</h3>
-        <div className="schedule-img-container">
+        <div id="schedule-img-container">
           <img src="https://firebasestorage.googleapis.com/v0/b/gallery-23854.appspot.com/o/peg3772.jpeg?alt=media&token=c204e58b-6ce0-4f34-97f3-92220aa54371" alt="glass-bar" />
           <img src="https://firebasestorage.googleapis.com/v0/b/gallery-23854.appspot.com/o/tancici-dum-3.jpeg?alt=media&token=887da6d7-65e5-42da-a02d-856d12ddda5c" alt="glass-bar" />
           <img src="https://firebasestorage.googleapis.com/v0/b/gallery-23854.appspot.com/o/A901-10-Things-you-did-not-know-about-Dancing-House-Rooftop-Image-1-1024x682.jpeg?alt=media&token=672f40fe-da52-467e-88fb-d410c4074ba1" alt="glass-bar" />
