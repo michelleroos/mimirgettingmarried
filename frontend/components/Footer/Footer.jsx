383a4c91ee34aadb2b91054aeecb20f9a81e0cdc
michelleroos <hello.michelleroos@gmail.com>
2022-03-27 19:29:09 -0400

    footer css

--- frontend/components/Footer/Footer.jsx
+++ frontend/components/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="footer">
-      Made by Michelle with <i className="fas fa-heart"></i> for Amir
+      <a href="https://github.com/michelleroos/mimirgettingmarried" target="_blank">
+        Made by Michelle with <i className="fas fa-heart"></i> for Amir
+      </a>
     </footer>
   )
 }
