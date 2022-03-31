45c08c18e09d3f1f6132280ce89c23f20eb845f5
michelleroos <hello.michelleroos@gmail.com>
2022-03-30 20:58:52 -0400

    schedule buttons

--- frontend/components/Schedule/Schedule.jsx
+++ frontend/components/Schedule/Schedule.jsx
@@ -35,16 +35,19 @@ export default function Schedule() {
         {ceremony && <Ceremony close={close} />}
         {reception && <Reception close={close} />}
         {sunday && <Sunday close={close} />}
+
         <div className="day-schedule">
           <h2>Friday - 22 July</h2>
           <div className="detailed-schedule-container">
             <div className="schedule-details" id="friday-modal" onClick={() => setFriday(true)}>
               <i className="fas fa-beer"></i>
               <h3>Meet & greet</h3>
-              <p className="hover">TBD</p>
+              <button className="more-info">More info</button>
+              {/* <p className="hover">TBD</p> */}
             </div>
           </div>
         </div>
+
         <div className="day-schedule">
           <h2>Saturday - 23 July</h2>
           <div className="detailed-schedule-container">
@@ -52,25 +55,27 @@ export default function Schedule() {
               <i className="fas fa-cocktail"></i>
               <h3>Mingle</h3>
               <p>15:00</p>
-              <p className="hover">Villa Richter Fountain</p>
+              <button className="more-info">More info</button>
+              {/* <p className="hover">Villa Richter Fountain</p> */}
             </div>
             <div className="schedule-details" id="saturday-ceremony-modal" onClick={() => setCeremony(true)}>
               <i className="fas fa-heart"></i>
               <h3>Ceremony</h3>
               <p>16:00</p>
-              <p className="hover">Villa Richter</p>
+              <button className="more-info">More info</button>
+              {/* <p className="hover">Villa Richter</p> */}
             </div>
             <div className="schedule-details" id="saturday-reception-modal" onClick={() => setReception(true)}>
               <i className="fas fa-utensils"></i>
               <h3>Reception</h3>
               <p>18:00</p>
-              <p className="hover">Villa Richter</p>
+              <button className="more-info">More info</button>
+              {/* <p className="hover">Villa Richter</p> */}
             </div>
             <div className="schedule-details">
               <i className="fas fa-guitar"></i>
               <h3>Music</h3>
               <p>20:00</p>
-              <p>Villa Richter</p>
             </div>
             <div className="schedule-details">
               <i className="fas fa-hourglass-end"></i>
@@ -79,6 +84,7 @@ export default function Schedule() {
             </div>
           </div>
         </div>
+        
         <div className="day-schedule">
           <h2>Sunday - 24 July</h2>
           <div className="detailed-schedule-container">
@@ -86,7 +92,8 @@ export default function Schedule() {
               <i className="fas fa-music"></i>
               <h3>Party continues</h3>
               <p>17:00 until late</p>
-              <p className="hover">GLASS Bar</p>
+              <button className="more-info">More info</button>
+              {/* <p className="hover">GLASS Bar</p> */}
             </div>
           </div>
         </div>
