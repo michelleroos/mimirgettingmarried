66959c5194549a63c698d4d69f007c67fd35ac11
michelleroos <hello.michelleroos@gmail.com>
2022-04-21 22:49:06 +0200

    add rsvps to ruby

---
+++ app/controllers/api/rsvps_controller.rb
@@ -0,0 +1,17 @@
+class Api::RsvpsController < ApplicationController
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  private
+
+  def rsvp_params
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@
 #    api_user GET    /api/users/:id(.:format) api/users#show {:format=>:json}
 #             PATCH  /api/users/:id(.:format) api/users#update {:format=>:json}
 #             PUT    /api/users/:id(.:format) api/users#update {:format=>:json}
+#   api_rsvps POST   /api/rsvps(.:format)     api/rsvps#create {:format=>:json}
+#    api_rsvp GET    /api/rsvps/:id(.:format) api/rsvps#show {:format=>:json}
+#             PATCH  /api/rsvps/:id(.:format) api/rsvps#update {:format=>:json}
+#             PUT    /api/rsvps/:id(.:format) api/rsvps#update {:format=>:json}
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -17,5 +21,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create, :show, :update]
+    resources :rsvps, only: [:create, :show, :update]
   end
 end
