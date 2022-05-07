3b28afffbd899b84b6ff55557ceae0410eab64b4
michelleroos <hello.michelleroos@gmail.com>
2022-05-07 15:02:19 -0400

    no sheets updates

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,24 +7,18 @@ import {
   HashRouter
 } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 import Faq from "./Faq/Faq";
 import Footer from "./Footer/Footer";
-import Location from "./Location/Location";
 import LoginFormContainer from "./Session/LoginFormContainer";
-// import NavbarContainer from "./Navbar/NavbarContainer";
 import Navbar from "./Navbar/Navbar";
 import RSVP from "./RSVP/Rsvp";
 import Schedule from "./Schedule/Schedule";
 import UploadForm from "./UploadForm/UploadForm";
 import WeddingParty from "./WeddingParty/WeddingParty";
 import Welcome from "./Welcome/Welcome";
-// import WelcomeContainer from "./Welcome/WelcomeContainer";
 
 export default function App() {
-
-  const [selectedImg, setSelectedImg] = useState(null);
 
   return (
     <div className="app">
