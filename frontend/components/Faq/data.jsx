b30d5f3673bada397ce865dfcfe54deb0262fbae
michelleroos <hello.michelleroos@gmail.com>
2022-05-08 19:57:30 -0400

    faq padding

--- frontend/components/Faq/data.jsx
+++ frontend/components/Faq/data.jsx
@@ -10,6 +10,14 @@ const data = [
     a: "July is the warmest month of the year in Prague with an average high-temperature of 24°C (75.2°F) and an average low-temperature of 11.9°C (53.4°F). Of course, in the last few years Europe has been significantly hotter in the summers. You might want to bring a hand fan in case it’s very hot in the day, and a light jacket for the night.",
   },
   {
+    q: "Where will the wedding take place?",
+    a: `Villa Richter is a classicist summerhouse from 1836 that is a part of the St Wenceslas Vineyard complex at Prague Castle. The complex offers a unique and undisturbed view of the panoramas of Old town, Lesser Town and the Villa Belvedere. The address is <a target="_blank" href="https://goo.gl/maps/NX8b6izLQu2a17vc7" target="_blank">Staré zámecké schody 6/251, 118 00 Praha 1-Hradčany</a>.`,
+  },
+  {
+    q: "How do I get to the venue and around Prague?",
+    a: `The venue is located in the city center so you can either walk, use the public transport or take a taxi. Please note there is an incline walk to get to the wedding if you choose to walk. If you are wearing heels or have reduced mobility, you might consider taking a taxi.`,
+  },
+  {
     q: "Will the ceremony/mingle/reception be in the same place?",
     a: "Yes.",
   },
@@ -18,13 +26,8 @@ const data = [
     a: "Weather permitting, the mingle and ceremony will be outside. The dinner and band will be in an indoor/outdoor area and the late night party will be inside.",
   },
   {
-    q: "How do I get to the venue and around Prague?",
-    a: `The address is <a target="_blank" href="https://goo.gl/maps/NX8b6izLQu2a17vc7" target="_blank">
-        Staré zámecké schody 6/251, 118 00 Praha 1-Hradčany</a>. The venue is located in the city center so you can either walk, use the public transport or take a taxi. Please note there is an incline walk to get to the wedding if you choose to walk. If you are wearing heels or have reduced mobility, you might consider taking a taxi.`,
-  },
-  {
     q: "Is the venue accessible for people with restricted mobility?",
-    a: " No, there are some stairs and no lift.",
+    a: " Not very. There are some stairs and no lift.",
   },
   {
     q: "Is there parking at the venue?",
@@ -32,11 +35,11 @@ const data = [
   },
   {
     q: "What time should I arrive?",
-    a: "TBC shortly but probably around 3pm.",
+    a: "TBC shortly but around 3pm.",
   },
   {
     q: "Are kids welcome?",
-    a: "Your young children are welcome.",
+    a: "Yes, your young children are welcome.",
   },
   {
     q: "Can I bring a +1?",
@@ -44,11 +47,11 @@ const data = [
   },
   {
     q: "Will there be any vegan/vegetarian/special diet options?",
-    a: `Yes, please let us know in your RSVP or by contacting <a target="_blank" href="https://wa.me/19178874963">Amir</a> or <a target="_blank" href="https://wa.me/16509332293">Michelle</a> if you need to be accommodated.`,
+    a: `Yes, please let us know in your <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeSWoMdSv030Q1_KpsrO0IZ0Qt2aYLa1arJ4rTWQUwm6ulbuw/viewform">RSVP</a> or by contacting <a target="_blank" href="https://wa.me/19178874963">Amir at +1 917 887 4963</a> or <a target="_blank" href="https://wa.me/16509332293">Michelle at +1 650 933 2293</a> if you need to be accommodated.`,
   },
   {
     q: "Can I post on social media?",
-    a: "Yes, of course! Just tag #mimirgettingmarried so that we can save the photos. While you’re at it, please add photos to our Google Photos album or send them to us via Apple Photos/DropBox/whichever way you want.",
+    a: `Yes, of course! We would love it if you could also add your photos to our <a target="_blank" href="https://photos.app.goo.gl/NW51qZ8HF3jVVEQ96">Google Photos album</a> or send them to us via Apple Photos/DropBox/whichever way you want.`,
   },
   {
     q: "Where do you recommend that I book my stay?",
@@ -62,10 +65,6 @@ const data = [
     q: "What should I do if I no longer can make it?",
     a: "Please just let us know asap so that we can plan accordingly.",
   },
-  // {
-  //   q: "I have another question!",
-  //   a: "Just reach out to us on WhatsApp - Michelle’s number is + 6509332293 and Amir’s is + 19178874963.",
-  // },
 ];
 
 export default data;
--- frontend/components/Schedule/Ceremony.jsx
+++ frontend/components/Schedule/Ceremony.jsx
@@ -6,7 +6,6 @@ export default function Ceremony({ close }) {
       <div className="schedule-modal" id="saturday-ceremony-modal">
         {/* <h2>Saturday - July 23</h2> */}
         <h2>Ceremony</h2>
-        <h3>Villa Richter is a classicist summerhouse from 1836 that is a part of the St Wenceslas Vineyard complex at Prague Castle. The complex offers a unique and undisturbed view of the panoramas of Old town, Lesser Town and the Villa Belvedere.</h3>
         <img src="https://firebasestorage.googleapis.com/v0/b/gallery-23854.appspot.com/o/SabrinaJan-057-2-1400x1050.jpeg?alt=media&token=4e03503e-5c38-4e60-915a-6051e609e94e" alt="saturday-ceremony" id="ceremony"/>
         <p>Photopraphy by <a href="http://www.exclusiveweddingsinprague.com/" target="_blank">Exclusive Weddings in Prague</a>.</p>
         <h3>Click <a href="https://www.google.com/maps/place/Villa+Richter/@50.0925169,14.406056,17z/data=!3m1!4b1!4m5!3m4!1s0x470b94e0852c61fb:0x4bb05a616b525517!8m2!3d50.0925169!4d14.4082447" target="_blank">here</a> for Villa Richter on Google Maps.</h3>
