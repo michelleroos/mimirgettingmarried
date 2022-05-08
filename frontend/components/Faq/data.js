ee4feeee204911cdfe88aa128808ff77ec191c2d
michelleroos <hello.michelleroos@gmail.com>
2022-05-07 21:25:43 -0400

    data

--- frontend/components/Faq/data.js
+++ frontend/components/Faq/data.js
@@ -1,68 +1,70 @@
+import React from "react";
+
 const data = [
   {
     q: "What should I wear?",
-    a: "The most important thing for us is that you’re comfortable! But expect the vibe to be a festive/cocktail/semi-formal fusion. The wedding party will wear sage/eucalyptus dresses and tan suits."
+    a: "The most important thing for us is that you’re comfortable! But expect the vibe to be a festive/cocktail/semi-formal fusion. The wedding party will wear sage/eucalyptus dresses and tan suits.",
   },
   {
     q: "What will the weather be like?",
-    a: "July is the warmest month of the year in Prague with an average high-temperature of 24°C (75.2°F) and an average low-temperature of 11.9°C (53.4°F). Of course, in the last few years Europe has been significantly hotter in the summers. You might want to bring a hand fan in case it’s very hot in the day, and a light jacket for the night."
+    a: "July is the warmest month of the year in Prague with an average high-temperature of 24°C (75.2°F) and an average low-temperature of 11.9°C (53.4°F). Of course, in the last few years Europe has been significantly hotter in the summers. You might want to bring a hand fan in case it’s very hot in the day, and a light jacket for the night.",
   },
   {
     q: "Will the ceremony/mingle/reception be in the same place?",
-    a: "Yes."
+    a: "Yes.",
   },
   {
     q: "Will the ceremony/drinks/reception be inside or outside?",
-    a: "Weather permitting, the mingle and ceremony will be outside. The dinner and band will be in an indoor/outdoor area and the late night party will be inside."
+    a: "Weather permitting, the mingle and ceremony will be outside. The dinner and band will be in an indoor/outdoor area and the late night party will be inside.",
   },
   {
     q: "How do I get to the venue and around Prague?",
-    a: "The venue is located in the city center so you can either walk, use the public transport or take a taxi. Please note there is an incline walk to get to the wedding if you choose to walk. If you are wearing heels or have reduced mobility, you might consider taking a taxi."
+    a: "The venue is located in the city center so you can either walk, use the public transport or take a taxi. Please note there is an incline walk to get to the wedding if you choose to walk. If you are wearing heels or have reduced mobility, you might consider taking a taxi.",
   },
   {
     q: "Is the venue accessible for people with restricted mobility?",
-    a: " No, there are some stairs and no lift."
+    a: " No, there are some stairs and no lift.",
   },
   {
     q: "Is there parking at the venue?",
-    a: "No."
+    a: "No.",
   },
   {
     q: "What time should I arrive?",
-    a: "TBC shortly but probably around 3pm."
+    a: "TBC shortly but probably around 3pm.",
   },
   {
     q: "Are kids welcome?",
-    a: "Your young children are welcome."
+    a: "Your young children are welcome.",
   },
   {
     q: "Can I bring a +1?",
-    a: "Sorry, no. We have a strict guest list to stay on budget and within capacity for the venue."
+    a: "Sorry, no. We have a strict guest list to stay on budget and within capacity for the venue.",
   },
   {
     q: "Will there be any vegan/vegetarian/special diet options?",
-    a: "Yes, please let us know in your RSVP or by contacting Michelle or Amir if you need to be accommodated."
+    a: `Yes, please let us know in your RSVP or by contacting Amir or Michelle if you need to be accommodated.`,
   },
   {
     q: "Can I post on social media?",
-    a: "Yes, of course! Just tag #mimirgettingmarried so that we can save the photos. While you’re at it, please add photos to our Google Photos album or send them to us via Apple Photos/DropBox/whichever way you want."
+    a: "Yes, of course! Just tag #mimirgettingmarried so that we can save the photos. While you’re at it, please add photos to our Google Photos album or send them to us via Apple Photos/DropBox/whichever way you want.",
   },
   {
     q: "Where do you recommend that I book my stay?",
-    a: "We recommend that you stay in Prague 1, which is the city center. There are plenty of options on Airbnb and Hotels/Booking."
+    a: "We recommend that you stay in Prague 1, which is the city center. There are plenty of options on Airbnb and Hotels/Booking.",
   },
   {
     q: "When is the RSVP deadline?",
-    a: "We appreciate you letting us know asap so that we can plan accordingly, but latest by May 15."
+    a: "We appreciate you letting us know asap so that we can plan accordingly, but latest by May 15.",
   },
   {
     q: "What should I do if I no longer can make it?",
-    a: "Please just let us know asap so that we can plan accordingly."
+    a: "Please just let us know asap so that we can plan accordingly.",
   },
   {
     q: "I have another question!",
-    a: "Just reach out to us on WhatsApp - Michelle’s number is + 6509332293 and Amir’s is + 19178874963."
-  }
-]
+    a: "Just reach out to us on WhatsApp - Michelle’s number is + 6509332293 and Amir’s is + 19178874963.",
+  },
+];
 
 export default data;
