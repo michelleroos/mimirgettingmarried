a737e3c54bee2c9023598be5b91c222b4f0cc4af
michelleroos <hello.michelleroos@gmail.com>
2022-07-17 09:44:19 -0400

    update order for FAQs

--- frontend/components/Faq/data.jsx
+++ frontend/components/Faq/data.jsx
@@ -2,22 +2,30 @@ import React from "react";
 
 const data = [
   {
+    q: "Where will the wedding take place?",
+    a: `Villa Richter is a classicist summerhouse from 1836 that is a part of the St Wenceslas Vineyard complex at Prague Castle. The complex offers a unique and undisturbed view of the panoramas of Old town, Lesser Town and the Villa Belvedere. The address is <a target="_blank" href="https://goo.gl/maps/NX8b6izLQu2a17vc7" target="_blank">Staré zámecké schody 6/251, 118 00 Praha 1-Hradčany</a>.`,
+  },
+  {
+    q: "What time should I arrive?",
+    a: "Please arrive at 3pm for welcome drinks. The ceremony will start at 4pm.",
+  },
+  {
+    q: "How do I get to there?",
+    a: `The venue is located close to the city center so you can either walk, use the public transport or take a taxi. 
+    
+    Taxi: tell them to stop by the <a target="_blank" href="https://www.google.com/maps/place/Old+Castle+Stairs/@50.092117,14.4054935,17z/data=!3m1!4b1!4m5!3m4!1s0x470b94e0608a878f:0x582a6234d68b495!8m2!3d50.092117!4d14.4076822" target="_blank">Old Castle Steps (Stare Zamecke Schody)</a>. There will be someone waiting there to direct you.`,
+  },
+  {
+    q: "Is there a gift registry?",
+    a: `We couldn’t think of a better way to start our marriage than by using our wedding to do a little good. So while we don’t expect wedding gifts, should you nevertheless like to give us something, we would be very grateful if you donated, <a target="blank" href="https://www.gofundme.com/f/michelle-amirs-wedding-charitable-giving?utm_campaign=p_lico+share-sheet&utm_medium=copy_link&utm_source=customer">via this website</a>, to GiveDirectly – a fantastic charity that sends money directly to the world’s poorest households. Using resources available to us, we will try to match the total amount collected.`,
+  },
+  {
     q: "What should I wear?",
     a: `The most important thing for us is that you’re comfortable! But expect the vibe to be a festive/cocktail/semi-formal fusion. The wedding party will wear sage/eucalyptus dresses and tan suits. The dress code for Sunday's party is <a target="_blank" href="https://www.google.com/search?q=studio+54+fashion&rlz=1C5CHFA_enUS927US929&sxsrf=ALiCzsZqxgt7IvcDyDJG0DONemzGVlCyjg:1658006821450&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjjwYLdrP74AhU-kIkEHdy8DXEQ_AUoAXoECAEQAw&biw=1600&bih=830&dpr=1.6" target="_blank">Studio 54</a>.`,
   },
   {
     q: "What will the weather be like?",
     a: "July is the warmest month of the year in Prague with an average high-temperature of 24°C (75.2°F) and an average low-temperature of 11.9°C (53.4°F). Of course, in the last few years Europe has been significantly hotter in the summers. You might want to bring a hand fan in case it’s very hot in the day, and a light jacket for the night.",
-  },
-  {
-    q: "Where will the wedding take place?",
-    a: `Villa Richter is a classicist summerhouse from 1836 that is a part of the St Wenceslas Vineyard complex at Prague Castle. The complex offers a unique and undisturbed view of the panoramas of Old town, Lesser Town and the Villa Belvedere. The address is <a target="_blank" href="https://goo.gl/maps/NX8b6izLQu2a17vc7" target="_blank">Staré zámecké schody 6/251, 118 00 Praha 1-Hradčany</a>.`,
-  },
-  {
-    q: "How do I get to the venue?",
-    a: `The venue is located close to the city center so you can either walk, use the public transport or take a taxi. 
-    
-    Taxi: tell them to stop by the <a target="_blank" href="https://www.google.com/maps/place/Old+Castle+Stairs/@50.092117,14.4054935,17z/data=!3m1!4b1!4m5!3m4!1s0x470b94e0608a878f:0x582a6234d68b495!8m2!3d50.092117!4d14.4076822" target="_blank">Old Castle Steps (Stare Zamecke Schody)</a>. There will be a hostess waiting.`,
   },
   {
     q: "Will the ceremony/mingle/reception be in the same place?",
@@ -36,10 +44,6 @@ const data = [
     a: "No.",
   },
   {
-    q: "What time should I arrive?",
-    a: "Please arrive at 3pm for welcome drinks. The ceremony will start at 4pm.",
-  },
-  {
     q: "Are kids welcome?",
     a: "Yes, your young children are welcome.",
   },
@@ -50,10 +54,6 @@ const data = [
   {
     q: "Will there be any vegan/vegetarian/special diet options?",
     a: `Yes, please let us know in your <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeSWoMdSv030Q1_KpsrO0IZ0Qt2aYLa1arJ4rTWQUwm6ulbuw/viewform">RSVP</a> or by contacting <a target="_blank" href="https://wa.me/19178874963">Amir at +1 917 887 4963</a> or <a target="_blank" href="https://wa.me/16509332293">Michelle at +1 650 933 2293</a> if you need to be accommodated.`,
-  },
-  {
-    q: "Is there a gift registry?",
-    a: `We couldn’t think of a better way to start our marriage than by using our wedding to do a little good. So while we don’t expect wedding gifts, should you nevertheless like to give us something, we would be very grateful if you donated, <a target="blank" href="https://www.gofundme.com/f/michelle-amirs-wedding-charitable-giving?utm_campaign=p_lico+share-sheet&utm_medium=copy_link&utm_source=customer">via this website</a>, to GiveDirectly – a fantastic charity that sends money directly to the world’s poorest households. Using resources available to us, we will try to match the total amount collected.`,
   },
   {
     q: "Can I post on social media?",
